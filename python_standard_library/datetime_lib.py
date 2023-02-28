caf55b13fdb8fbd641b80ae676cda57804697245
Jonathan <Zweitonegoismus@gmail.com>
2023-02-28 13:12:01 -0500

    Datetime

--- python_standard_library/datetime_lib.py
+++ python_standard_library/datetime_lib.py
@@ -1,3 +1,19 @@
-import datetime
+from datetime import datetime, timedelta
+import time
 
-dt= datetime.datetime(2018, 1, 1)
+
+dt = datetime(2018, 1, 1) + timedelta(days=1, seconds=1000)
+dt_now = datetime.now()
+dt_strp = datetime.strptime("2018/01/01", "%Y/%m/%d")
+dt_from = datetime.fromtimestamp(time.time())
+print(dt_from)
+
+print(f"{dt.year}/{dt.month}")
+print(dt_from.strftime("%Y/%m"))
+
+
+duration = dt_now - dt
+print(duration)
+print("days", duration.days)
+print("seconds", duration.seconds)
+print("total_seconds", duration.total_seconds())
